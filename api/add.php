49d7d4309bc36006da95158ff6f69cd8ea470ba4
ns130291 <ns130291@web.de>
2015-10-18 21:51:17 +0200

    added missing error handling and escaping

--- api/add.php
+++ api/add.php
@@ -5,27 +5,32 @@
 $datum = '"' . $_POST["datum"] . '"';
 $kategorie = "";
 if (isset($_POST["kategorie"])) {
-    $kategorie = '"' . urldecode($_POST["kategorie"]) . '"';
+    $kategorie = '"' . $mysqli->real_escape_string(urldecode($_POST["kategorie"])) . '"';
 } else {
     $kategorie = "null";
 }
-$art = '"' . urldecode($_POST["art"]) . '"';
+$art = '"' . $mysqli->real_escape_string(urldecode($_POST["art"])) . '"';
 $preis = $_POST["preis"];
 $beschreibung = "";
 if (isset($_POST["beschreibung"])) {
-    $beschreibung = '"' . urldecode($_POST["beschreibung"]) . '"';
+    $beschreibung = '"' . $mysqli->real_escape_string(urldecode($_POST["beschreibung"])) . '"';
 } else {
     $beschreibung = "null";
 }
+$query = sprintf('CALL eua.ausgabeSpeichern(%s,%s,%s,%s,%s,%s);', $datum, $kategorie, $art, $preis, $beschreibung, $_SESSION['defaultKonto']);
+$result = $mysqli->query($query);
+if ($result) {
+    $insertId = $mysqli->insert_id;
 
-$mysqli->query(sprintf('CALL eua.ausgabeSpeichern(%s,%s,%s,%s,%s,%s);', $datum, $kategorie, $art, $preis, $beschreibung, $_SESSION['defaultKonto']));
-$insertId = $mysqli->insert_id;
-if ($insertId == 0) {
-    $result = $mysqli->query('SELECT MAX(idausgabe) as insertid FROM ausgabe;');
-    if ($result) {
-        $row = $result->fetch_assoc();
-        $insertId = $row['insertid'];
+    if ($insertId == 0) {
+        $result = $mysqli->query('SELECT MAX(idausgabe) as insertid FROM ausgabe;');
+        if ($result) {
+            $row = $result->fetch_assoc();
+            $insertId = $row['insertid'];
+        }
     }
+    echo '{"id":"' . $insertId . '"}';
+} else {
+    echo('{"error":"server","msg":"Insert error"}');
 }
-echo '{"id":"' . $insertId . '"}';
 
